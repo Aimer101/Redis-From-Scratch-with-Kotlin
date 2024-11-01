1fd513620d189cc15ff7c11a1a32d99a2e831451
Muaz Zakaria <muazzakaria@MacBook-Air-von-MacSzerezcom.local>
2024-11-01 12:18:41 +0100

    handle echo command

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -21,26 +21,45 @@ fun main(args: Array<String>) {
 fun handleClient(client : Socket) {
     val inputClient = client.getInputStream()
     val outputClient = client.getOutputStream()
+    var bytesRead: Int
+    val buffer = ByteArray(1024);
 
     try {
         while(client.isConnected) {
-            val request = inputClient.bufferedReader()
-            val requestBody = request.readLine() ?: ""
-            if(requestBody.isEmpty()) {
+            bytesRead = inputClient.read(buffer)
+
+            if (bytesRead == -1) {
                 break
             }
-            outputClient.write("+PONG\r\n".toByteArray())
-            outputClient.flush()
+
+            val request = buffer.copyOfRange(0, bytesRead).toString(Charsets.UTF_8);
+            val requestParts = RedisRequestProcessor().processRequest(request)
+            System.out.println("requestParts size: " + requestParts.size)
+            if(requestParts.isEmpty()) {
+                outputClient.write("-ERR\r\n".toByteArray())
+            } else if (requestParts[0] == "PING") {
+                outputClient.write("+PONG\r\n".toByteArray())
+            } else if (requestParts[0] == "ECHO") {
+                for (i in 1 until requestParts.size) {
+                    println(requestParts[i])
+                    outputClient.write("+${requestParts[i]}\r\n".toByteArray())
+                }
+            }
+
+            output.flush()
+
+
+            // val request = inputClient.bufferedReader()
+            // val requestBody = request.readLine() ?: ""
+            // if(requestBody.isEmpty()) {
+            //     break
+            // }
+            // outputClient.write("+PONG\r\n".toByteArray())
         }
     } catch (e: Exception) {
         println("Error handling client: ${e.message}")
     } finally {
-        // Ensure the client socket is closed in the end
-        try {
-            client.close()
-            println("Client connection closed: ${client.inetAddress}")
-        } catch (e: Exception) {
-            println("Error closing client connection: ${e.message}")
-        }
+        inputClient.close()
+        outputClient.close()
     }
 }
---
+++ src/main/kotlin/RedisRequestProcessor.kt
@@ -0,0 +1,28 @@
+class RedisRequestProcessor {
+    private val ASTERISK = '*';
+    fun processRequest(request: String): List<String> {
+        // Process the request and create a RedisRequest object
+        // This is a placeholder implementation
+        // *2\r\n$4\r\nECHO\r\n$9\r\nraspberry
+        val parsedRequestParts = ArrayList<String>();
+        if(request.startsWith(ASTERISK)){
+            val requestParts = request.split("\r\n")
+            // [*2, $4, ECHO, $9, raspberry]
+
+            // to get total args
+            requestParts[0] = requestParts[0].replace("*","")
+            var numParts = requestParts[0].toInt()
+
+            System.out.println("nArgs: "+numParts)
+
+            var index = 2;
+
+            while(numParts > 0){
+                parsedRequestParts.add(requestParts[index])
+                index+=2;
+                numParts--;
+            }
+        }
+        return parsedRequestParts;
+    }
+}
