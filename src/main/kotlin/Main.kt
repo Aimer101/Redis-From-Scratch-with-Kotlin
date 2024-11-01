d24a8d303acab9dbeda00c822da8ff291b44de6b
Muaz Zakaria <muazzakaria@MacBook-Air-von-MacSzerezcom.local>
2024-11-01 10:27:58 +0100

    accept multiple connection concurrently

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,7 @@ import java.net.ServerSocket
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
+import java.net.Socket 
 
 fun main(args: Array<String>) {
     var serverSocket = ServerSocket(6379)
