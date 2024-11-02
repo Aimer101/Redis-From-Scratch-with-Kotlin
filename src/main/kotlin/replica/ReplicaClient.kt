71433f023d547367ccac89c97718ddac66c8639b
Muaz Zakaria <muazzakaria@MacBook-Air-von-MacSzerezcom.local>
2024-11-02 18:11:56 +0100

    send replconf to master

--- src/main/kotlin/replica/ReplicaClient.kt
+++ src/main/kotlin/replica/ReplicaClient.kt
@@ -3,9 +3,10 @@ import java.io.PrintWriter
 import java.io.BufferedReader
 import java.io.InputStreamReader
 
-class ReplicaClient (host : String, port : Int) {
+class ReplicaClient (private val host : String, private val port : Int) {
     private val socket: Socket = Socket(host, port)
     private val outputClient = PrintWriter(socket.getOutputStream(), true)
+    private val request = BufferedReader(InputStreamReader(socket.getInputStream()))
 
     fun ping () {
         println("Sending ping to master")
@@ -13,18 +14,16 @@ class ReplicaClient (host : String, port : Int) {
         outputClient.flush()
         println("Ping sent to master")
 
-        val request = BufferedReader(InputStreamReader(socket.getInputStream()))
         val response = request.readLine()
         println("Response from master for ping: $response")
     }
 
     fun replconf() {
         println("Sending replconf 1 to master")
-        outputClient.print("*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n${port.length}\r\${port}\r\n")
+        outputClient.print("*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n${port.toString().length}\r\${port}\r\n")
         outputClient.flush()
         println("Replconf 1 sent to master")
 
-        val request = BufferedReader(InputStreamReader(socket.getInputStream()))
         val response = request.readLine()
         println("Response from master for replconf 1: $response")
 
