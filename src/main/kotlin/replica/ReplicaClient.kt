d69dd849a1bb22623440df89d663a7bdbe553f3e
Muaz Zakaria <muazzakaria@MacBook-Air-von-MacSzerezcom.local>
2024-11-02 17:59:54 +0100

    send ping to master

--- src/main/kotlin/replica/ReplicaClient.kt
+++ src/main/kotlin/replica/ReplicaClient.kt
@@ -1,5 +1,7 @@
 import java.net.Socket
 import java.io.PrintWriter
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 class ReplicaClient (host : String, port : Int) {
     private val socket: Socket = Socket(host, port)
@@ -10,5 +12,9 @@ class ReplicaClient (host : String, port : Int) {
         outputClient.print("*1\r\n$4\r\nPING\r\n")
         outputClient.flush()
         println("Ping sent to master")
+
+        val request = BufferedReader(InputStreamReader(socket.getInputStream()))
+        val response = request.readLine()
+        println("Response from master: $response")
     }
 }
