6812b66f4015f0449ee9a3cf14bd619a1962c4b2
Muaz Zakaria <muazzakaria@MacBook-Air-von-MacSzerezcom.local>
2024-11-02 17:55:32 +0100

    send ping to master

--- src/main/kotlin/replica/ReplicaClient.kt
+++ src/main/kotlin/replica/ReplicaClient.kt
@@ -7,8 +7,7 @@ class ReplicaClient (host : String, port : Int) {
 
     fun ping () {
         println("Sending ping to master")
-        outputClient.print("*1\r\n$4\r\nPING\r\n")
-        outputClient.flush()
+        outputClient.println("*1\r\n$4\r\nPING\r\n")
         println("Ping sent to master")
     }
 }
